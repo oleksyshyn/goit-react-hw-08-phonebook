265060b883ea427582f4effa755ecaa99a33a7a5
Iryna Oleksyshyn <irynkaoleksyshyn@gmail.com>
2023-07-24 19:12:43 +0300

    Fixed RestrictedRoute

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,25 +8,16 @@ import RegisterPage from 'pages/registerPage';
 import LoginPage from 'pages/loginPage';
 import { refreshUser } from 'redux/auth/operations';
 import { useAuth } from 'hooks/index';
-// import RestrictedRoute from 'routes/RestrictedRoute';
-
+import RestrictedRoute from 'routes/RestrictedRoute';
 import ProtectedRoute from 'routes/ProtectedRoute';
-// import { useNavigate } from 'react-router-dom';
 
 function App() {
   const { isRefreshing, isLoggedIn } = useAuth();
   const dispatch = useDispatch();
-  // const navigate = useNavigate();
 
   useEffect(() => {
     dispatch(refreshUser());
   }, [dispatch]);
-
-  // useEffect(() => {
-  //   if (isLoggedIn) {
-  //     navigate('/contacts');
-  //   } 
-  // }, [isLoggedIn, navigate]);
  
   return isRefreshing ? (<div>Loading...</div>) : (
     <div>
@@ -34,11 +25,18 @@ function App() {
         <Route path="/" element={<SharedLayout />}>
           <Route index element={<Home />} />
           <Route path="/contacts" element={ <ProtectedRoute isLoggedIn={isLoggedIn}><Contacts/></ProtectedRoute>} />
-          {/* <Route path="/register" element={<RestrictedRoute  component={<RegisterPage />} />} redirectTo="/contacts" /> */}
-          {/* <Route path="/login" element={<RestrictedRoute component={<LoginPage />} redirectTo="/contacts"/>} /> */}
-          {/* <Route path="/contacts" element={ <Contacts/>} /> */}
-          <Route path="/register" element={<RegisterPage/>} />
-          <Route path="/login" element={<LoginPage/>} />
+          <Route
+            path="/register"
+            element={
+              <RestrictedRoute component={RegisterPage} redirectTo="/contacts" />
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <RestrictedRoute component={LoginPage} redirectTo="/contacts" />
+            }
+          />
         </Route>
         {/* <Route path="*" element={ <NotFound />} /> */}
       </Routes>
--- src/routes/RestrictedRoute.js
+++ src/routes/RestrictedRoute.js
@@ -1,11 +1,10 @@
-import { Navigate } from 'react-router-dom';
 import { useAuth } from 'hooks/index';
+import { Navigate } from 'react-router-dom';
 
-function RestrictedRoute({ component: Component, redirectTo = "/" }) {
-    const isLoggedIn = useAuth();
+const RestrictedRoute = ({ component: Component, redirectTo = '/' }) => {
+    const { isLoggedIn } = useAuth();
 
-    return isLoggedIn ? <Navigate to={redirectTo} /> : Component;
-  
-}
+  return isLoggedIn ? <Navigate to={redirectTo} /> : <Component />;
+};
 
 export default RestrictedRoute;
